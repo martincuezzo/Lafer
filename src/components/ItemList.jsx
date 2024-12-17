eba7e49017d1f5af5b24b4fff64ddc5e460d4b12
martincuezzo <martindiazcuezzo@gmail.com>
2024-12-17 17:12:39 -0300

    ajustes de estilo

--- src/components/ItemList.jsx
+++ src/components/ItemList.jsx
@@ -3,7 +3,7 @@ import Item from './Item'
 
 const ItemList = ({products}) => {
   return (
-    <div className='d-flex justify-content-around align-items-center flex-wrap gap-4 mt-4'>
+    <div className='d-flex justify-content-around align-items-center flex-wrap gap-4 mt-4 min-vh-100'>
         {products.map((product)=> <Item product={product} key={product.id}/>)}
     </div>
   )
